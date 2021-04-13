bf189ab06220a1f0379d78e18fe034f24f8280d0
Ruben Nogueira <ruben.p.nogueira@gmail.com>
2021-04-13 17:54:19 +0100

    Fix withCookies without ES6

--- src/middleware/withCookies.js
+++ src/middleware/withCookies.js
@@ -5,7 +5,10 @@ const withCookies = request => {
     request.cookies = (request.headers.get('Cookie') || '')
       .split(/;\s*/)
       .map(pair => pair.split('='))
-      .reduce((acc, [key, value]) => (acc = {...acc, [key]: value}, {})
+      .reduce((acc, [key, value]) => {
+        acc[key] = value;
+        return acc;
+      }, {})
   } catch (err) {}
 }
 
